57d8b46e51a56f2fc56c0b492a04036db8c7cb18
Emily McGovern <mcg21013@byui.edu>
2025-01-11 12:44:43 -0700

    Adding the C# Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNum = int.Parse(grade);
+
+        string letter = "";
+
+        if (gradeNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (gradeNum >= 70)
+        {
+            Console.WriteLine("You Passed!!!!:)");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Good luck next time!");
+        }
+
     }
 }
